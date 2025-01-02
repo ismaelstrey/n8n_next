49a9915259b7706046d6c6c9721c6a17fd9ecc6c
ismael strey pereira <ismaelstrey@hotmail.com>
2025-01-02 14:32:03 -0300

    🛠️ package-lock.json -> Added @types/node-cron dependency 🛠️ package.json -> Added @types/node-cron dependency 🛠️ src/app/api/route.ts -> Refactored cronJob function 🛠️ src/app/api/route.ts -> Enhanced error handling 🛠️ src/app/api/route.ts -> Added GET function for webhook 🛠️ src/app/page.tsx -> Removed cronJob invocation from Home

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,8 +1,10 @@
-const cron = require('node-cron');
-const axios = require('axios');
+import { NextResponse } from "next/server";
+import cron from "node-cron";
+import axios from "axios";
+
 
 // Executa o webhook a cada minuto
-export const cronJob = async () => {
+async function cronJob(): Promise<void> {
     cron.schedule('* * * * *', async () => {
         try {
             console.log('Executando o webhook...');
@@ -10,10 +12,22 @@ export const cronJob = async () => {
                 message: 'Este é um teste do cron job'
             });
             console.log('Resposta do Webhook:', response.data);
-        } catch (error: any) {
-            console.error('Erro ao executar o webhook:', error.message);
+        } catch (error: unknown) {
+            if (axios.isAxiosError(error)) {
+                console.error('Erro ao executar o webhook:', error.message);
+            } else {
+                console.error('Erro desconhecido:', error);
+            }
         }
     });
 
     console.log('Cron job configurado. O webhook será executado automaticamente.');
+}
+
+
+export async function GET() {
+    console.log('Executando GET');
+    await cronJob()
+    NextResponse.json({ message: 'Hello, World!', statusCode: 200 });
+
 }
