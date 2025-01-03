832e316ba2b06da0f3bc67b8107915121d3b688a
ismael strey pereira <ismaelstrey@hotmail.com>
2025-01-03 08:59:55 -0300

    apdate api cron for vercel

---
+++ src/app/api/cron/route.ts
@@ -0,0 +1,35 @@
+import { NextResponse } from "next/server";
+import cron from "node-cron";
+import axios from "axios";
+
+
+// Executa o webhook a cada minuto
+async function cronJob(): Promise<void> {
+    try {
+        console.log('Executando o webhook...');
+        const response = await axios.post('https://n8n.strey.com.br/webhook/d78059a9-5d1a-468a-9d6f-d8a98d8c8147', {
+            message: 'Este é um teste do cron job'
+        });
+        console.log('Resposta do Webhook:', response.data);
+    } catch (error: unknown) {
+        if (axios.isAxiosError(error)) {
+            console.error('Erro ao executar o webhook:', error.message);
+        } else {
+            console.error('Erro desconhecido:', error);
+        }
+    }
+
+    console.log('Cron job configurado. O webhook será executado automaticamente.');
+}
+await cronJob()
+
+export async function GET() {
+    console.log('Executando GET');
+    await cronJob()
+    return NextResponse.json({ success: true, message: 'Executando GET', statusCode: 200 }, {
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
+    });
+
+}
