fa031a8c14bc3e738cd0765f538c8df9e4f9926f
ismael strey pereira <ismaelstrey@hotmail.com>
2025-01-03 09:05:13 -0300

    update api

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,33 +1,7 @@
 import { NextResponse } from "next/server";
-import cron from "node-cron";
-import axios from "axios";
-
-
-// Executa o webhook a cada minuto
-async function cronJob(): Promise<void> {
-    cron.schedule('* * * * *', async () => {
-        try {
-            console.log('Executando o webhook...');
-            const response = await axios.post('https://n8n.strey.com.br/webhook/d78059a9-5d1a-468a-9d6f-d8a98d8c8147', {
-                message: 'Este é um teste do cron job'
-            });
-            console.log('Resposta do Webhook:', response.data);
-        } catch (error: unknown) {
-            if (axios.isAxiosError(error)) {
-                console.error('Erro ao executar o webhook:', error.message);
-            } else {
-                console.error('Erro desconhecido:', error);
-            }
-        }
-    });
-
-    console.log('Cron job configurado. O webhook será executado automaticamente.');
-}
-
 
 export async function GET() {
     console.log('Executando GET');
-    await cronJob()
     return NextResponse.json({ success: true, message: 'Executando GET', statusCode: 200 }, {
         headers: {
             'Access-Control-Allow-Origin': '*',
